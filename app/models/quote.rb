86f14618915416188856edec1342b1aa923647b8
kamkara <lnclass.kamkara@gmail.com>
2023-03-30 17:59:10 +0000

    app: Add real Time

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,10 @@ class Quote < ApplicationRecord
 
     scope :ordered, -> {order(id: :desc)}
     # Ex:- scope :active, -> {where(:active => true)}
+
+    #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
+    after_create_commit -> { broadcast_prepend_to "quotes"}
+    after_update_commit -> { broadcast_replace_to "quotes"}
+    after_destroy_commit -> { broadcast_remove_to "quotes"}
 end
