ca6aa668382f067ab0d3127e017ca9e954cceae7
kamkara <lnclass.kamkara@gmail.com>
2023-04-05 15:09:04 +0000

    Add Total Quote

--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,6 +1,12 @@
 require "application_system_test_case"
 
 class LineItemDatesTest < ApplicationSystemTestCase
+
+  # We must include this module to be able to use the
+  # `number_to_currency` method in our test
+  include ActionView::Helpers::NumberHelper
+
+
    setup do
     login_as users(:accountant)
 
@@ -46,6 +52,7 @@ test "Destroying a line item date" do
   end
 
   assert_no_text I18n.l(Date.current, format: :long)
+  assert_text number_to_currency(@quote.total_price)
 end
 
 end
